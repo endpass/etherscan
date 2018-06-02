cc7a8d15a03f6fd3ac02eaee07370a122a606a02
Dutch <dutch@unviral.com>
2018-06-01 20:23:27 -0400

    Refactor context to separate methods

--- account.go
+++ account.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"errors"
 	"math/big"
+	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Response with balance of a single address
@@ -33,15 +35,21 @@ func parseBalanceResponse(data []byte) (*big.Int, error) {
 	return bal, nil
 }
 
-// Balance returns the balance of a single address
-func (c *Client) Balance(ctx context.Context, addr string) (*big.Int, error) {
+func (c *Client) buildBalanceRequest(addr string) (*http.Request, error) {
+	if !strings.HasPrefix(addr, "0x") {
+		return nil, errors.New("Address must begin with 0x")
+	}
 	params := url.Values{}
 	params.Set("module", "account")
 	params.Set("action", "balance")
 	params.Set("tag", "latest")
 	params.Set("address", addr)
 
-	req, err := c.buildRequest(params)
+	return c.buildRequest(params)
+}
+
+func (c *Client) balance(ctx context.Context, addr string) (*big.Int, error) {
+	req, err := c.buildBalanceRequest(addr)
 	if err != nil {
 		return nil, err
 	}
@@ -49,7 +57,16 @@ func (c *Client) Balance(ctx context.Context, addr string) (*big.Int, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	return parseBalanceResponse(data)
+}
 
+// Balance returns the balance of a single address
+func (c *Client) Balance(addr string) (*big.Int, error) {
+	return c.balance(context.Background(), addr)
+}
+
+// BalanceContext returns the balance of a single address with a custom
+// context
+func (c *Client) BalanceContext(ctx context.Context, addr string) (*big.Int, error) {
+	return c.balance(ctx, addr)
 }
--- contract.go
+++ contract.go
@@ -3,7 +3,10 @@ package etherscan
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Response with balance of a single address
@@ -24,15 +27,20 @@ func parseABIResponse(data []byte) ([]byte, error) {
 	return res.Data, nil
 }
 
-// ContractABI returns the raw, unparsed ABI definition for a smart contract
-// at the given address
-func (c *Client) ContractABI(ctx context.Context, addr string) ([]byte, error) {
+func (c *Client) buildContractABIRequest(addr string) (*http.Request, error) {
+	if !strings.HasPrefix(addr, "0x") {
+		return nil, errors.New("Address must begin with 0x")
+	}
 	params := url.Values{}
 	params.Set("module", "contract")
 	params.Set("action", "getabi")
 	params.Set("address", addr)
 
-	req, err := c.buildRequest(params)
+	return c.buildRequest(params)
+}
+
+func (c *Client) contractABI(ctx context.Context, addr string) ([]byte, error) {
+	req, err := c.buildContractABIRequest(addr)
 	if err != nil {
 		return nil, err
 	}
@@ -40,6 +48,17 @@ func (c *Client) ContractABI(ctx context.Context, addr string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	return parseABIResponse(data)
+}
+
+// ContractABI returns the raw, unparsed ABI definition for a smart contract
+// at the given address
+func (c *Client) ContractABI(addr string) ([]byte, error) {
+	return c.contractABI(context.Background(), addr)
+}
+
+// ContractABI returns the raw, unparsed ABI definition for a smart contract
+// at the given address with a custom context
+func (c *Client) ContractABIContext(ctx context.Context, addr string) ([]byte, error) {
+	return c.contractABI(ctx, addr)
 }
